91a760c5af016ecff82b0c129456f8a18bbf7041
EmersonSantana1994 <87781497+EmersonSantana1994@users.noreply.github.com>
2021-08-05 00:13:53 -0300

    Add files via upload
    test

--- Exercicio1.java
+++ Exercicio1.java
@@ -2,8 +2,25 @@
 public class Exercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		int numero = 1000;
+		
+		
+		
+		
+		for (int x = 0;x <= 999; x++) {
+				
+			numero += 1;
+		
+			if (numero % 11 == 5 ) {
+				
+				System.out.println(numero);
+			}
+		
+		}
+		
 	}
-
+	
 }
+

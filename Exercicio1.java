19d214b4a86af8361eec2d643efbfef6f7ba8f1a
EmersonSantana1994 <87781497+EmersonSantana1994@users.noreply.github.com>
2021-08-05 00:17:13 -0300

    Add files via upload
    Exercicios Java - Laços de Repetição

---
+++ Exercicio1.java
@@ -0,0 +1,26 @@
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+
+		int numero = 1000;
+		
+		
+		
+		
+		for (int x = 0;x <= 999; x++) {
+				
+			numero += 1;
+		
+			if (numero % 11 == 5 ) {
+				
+				System.out.println(numero);
+			}
+		
+		}
+		
+	}
+	
+}
+

2b40cf5d3c77fe71d0b324290753e08ab044b637
EmersonSantana1994 <87781497+EmersonSantana1994@users.noreply.github.com>
2021-08-05 00:19:42 -0300

    Update Exercico5.java
    Exercicio Java - Laços de Repetição

--- Exercico5.java
+++ Exercico5.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
 
-public class Exercico5 extends SegundoPrograa {
-
+public class Exercico5 {
+	
+		public static void main(String[] args) {
+			
+			Scanner leia = new Scanner(System.in);
+			int numero = 0;
+			int cont = 0;
+			
+			do {
+				System.out.println("Digite o numero ");
+				numero = leia.nextInt();
+				cont += numero;
+			}
+				
+			
+				
+				while (numero != 0);
+							
+						System.out.println("A soma dos numero é " + cont );
+						
+				
+			
+				
+			
+			
+		
+		
+	
+		
 }
+
+		}
